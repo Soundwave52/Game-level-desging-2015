ee0b33d26ed5cf0a1339b56024673528a6525fed
Soundwave52 <soundwave52@sbcglobal.net>
2015-09-23 14:25:03 -0500

    New save
    A small update added ai nothing major

---
+++ Assets/Scrips Folder/Moveto.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moveto : MonoBehaviour {
+
+
+	public Transform goal;
+	NavMeshAgent agent;
+
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+	}
+	
+
+	void Update () {
+		agent.destination = goal.position;
+	}
+}
